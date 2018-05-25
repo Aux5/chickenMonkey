7ab6253ac0cb3f76faf3c2417d987b52c69b5036
Brett Shearin <brettcshearin@hotmail.com>
2018-05-25 10:20:36 -0500

    first commit

---
+++ BattleOfTheBands.js
@@ -0,0 +1,13 @@
+let bandNumber = (1)
+
+const takeNumber = function (Bandname ) {
+
+    return `${bandNumber++}. ${Bandname}`
+    
+}
+const scum = takeNumber("Galactic Scum")
+console.log(scum)
+ const under = takeNumber("Underdogs")
+ console.log(under)
+ const over = takeNumber("FINNALLY")
+ console.log(over)
